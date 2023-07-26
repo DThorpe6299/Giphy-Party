a2330d7bcac15296cdd75b1f94451d189c2c4942
DThorpe6299 <120077813+DThorpe6299@users.noreply.github.com>
2023-07-26 18:13:35 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-$gifArea = $('#gif-area');
-$searchInput = $('#search');
+$gifArea = $("#gif-area");
+$searchInput = $("#search");
 
 function addGif(res) {
-    let numResults = res.data.length;
+    let numResults = res.data.images.original.length;
     if(numResults) {
         let randomGif = Math.floor(Math.random() * numResults);
         let $newCol = $('<div>', {class: "col-md-4 col-12 mb-4"});
         let $newGif = $("<img>", {
-            scr: res.data[randomGif].images.original.url,
+            scr: res.data.images.original[randomGif].url,
             class: "w-100"
         });
         $newCol.append($newGif);
@@ -15,18 +15,22 @@ function addGif(res) {
     }
 }
 
-$("form").on("click", async function() {
-    let searchTerm = $searchInput.val();
-    $searchInput.val("");
 
-    const response = await axios.get('http://api.giphy.com/v1/gifs/search', { params: {
+$("#search-btn").on("submit", async function(evt) {
+    evt.preventDefault();
+    let searchTerm = $searchInput.val();
+
+    const response = await axios.get("https://api.giphy.com/v1/gifs/search?api_key=VmrAea42c0MbVP3POL0XdrE5YXkhpZIn&q=&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips", { params: {
         q: searchTerm,
-        api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        api_key: "VmrAea42c0MbVP3POL0XdrE5YXkhpZIn"
     }
     });
     addGif(response.data);
+    console.log("Added Gif!");
+    $searchInput.val("");
+    
 });
 
-$('#remove').on("click", function(){
+$('#remove-btn').on("click", function(){
     $gifArea.empty();
 })
